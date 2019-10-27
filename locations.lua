2e850b201d822c2dc57e5b27a4e7aea00e3f581f
Nkfree <BlackVaclav@gmail.com>
2019-10-27 12:26:46 +0100

    Allows for locations to work properly with Partyhealth
    Put it in server/scripts/

---
+++ locations.lua
@@ -0,0 +1,208 @@
+-- names of places/towns are extracted from openmw-cs
+
+local locations = {}
+
+
+locations["11, 16"] = "Ahemmusa Camp"
+
+locations["11, 20"] = "Ald Daedroth"
+
+locations["-4, 21"] = "Ald Redaynia"
+
+locations["6, -9"] = "Ald Sotha"
+
+locations["-11, 15"] = "Ald Velothi"
+
+locations["-2, 6"] = "Ald-ruhn"
+locations["-2, 7"] = "Ald-ruhn"
+locations["-3, 6"] = "Ald-ruhn"
+
+locations["-9, 5"] = "Andasreth"
+
+locations["2, -6"] = "Arvel Plantation"
+
+locations["-10, 15"] = "Ashalmawia"
+
+locations["5, 18"] = "Ashurnabitashpi"
+
+locations["-7, -4"] = "Ashurnibibi"
+
+locations["8, -12"] = "Bal Fell"
+locations["9, -12"] = "Bal Fell"
+
+locations["-5, 9"] = "Bal Isra"
+
+locations["6, -5"] = "Bal Ur"
+
+locations["-2, -2"] = "Balmora"
+locations["-3, -2"] = "Balmora"
+locations["-3, -3"] = "Balmora"
+locations["-4, -2"] = "Balmora"
+
+locations["-10, 9"] = "Berandas"
+
+locations["-2, 5"] = "Buckmoth Legion Fort"
+
+locations["-2, 2"] = "Caldera"
+
+locations["7, 22"] = "Dagon Fel"
+
+locations["2, 8"] = "Dagoth Ur"
+
+locations["2, -7"] = "Dren Plantation"
+
+locations["1, -13"] = "Ebonheart"
+locations["2, -13"] = "Ebonheart"
+
+locations["13, -1"] = "Erabenimsun Camp"
+
+locations["-2, 15"] = "Falasmaryon"
+
+locations["9, 6"] = "Falensarano"
+
+locations["1, -5"] = "Fields of Kummu"
+
+locations["-22, 16"] = "Fort Frostmoth"
+locations["-22, 17"] = "Fort Frostmoth"
+
+locations["2, 4"] = "Ghostgate"
+locations["-8, 3"] = "Gnaar Mok"
+
+locations["-10, 11"] = "Gnisis"
+locations["-11, 10"] = "Gnisis"
+locations["-11, 11"] = "Gnisis"
+
+locations["-6, -5"] = "Hla Oad"
+
+locations["-6, -1"] = "Hlormaren"
+
+locations["19, -4"] = "Holamayan"
+
+locations["-9, 4"] = "Khartag Point"
+
+locations["-9, 16"] = "Khuul"
+locations["-9, 17"] = "Khuul"
+
+locations["-11, 9"] = "Koal Cave Entrance"
+
+locations["0, 14"] = "Kogoruhn"
+
+locations["-3, 12"] = "Maar Gan"
+
+locations["4, -3"] = "Marandus"
+
+locations["12, -8"] = "Molag Mar"
+locations["13, -8"] = "Molag Mar"
+
+locations["-1, -3"] = "Moonmoth Legion Fort"
+
+locations["14, -4"] = "Mount Assarnibibi"
+
+locations["11, -5"] = "Mount Kand"
+
+locations["8, -10"] = "Mzahnch Ruin"
+
+locations["6, 21"] = "Mzuleft Ruin"
+
+locations["8, 12"] = "Nchuleft Ruin"
+
+locations["10, -3"] = "Nchuleftingth"
+
+locations["17, -6"] = "Nchurdamz"
+
+locations["-5, -5"] = "Odai Plateau"
+
+locations["3, 7"] = "Odrosal"
+
+locations["0, -7"] = "Pelagiad"
+locations["0, -8"] = "Pelagiad"
+
+locations["-24, 19"] = "Raven Rock"
+locations["-25, 19"] = "Raven Rock"
+
+locations["6, 18"] = "Rotheran"
+
+locations["17, 4"] = "Sadrith Mora"
+locations["17, 5"] = "Sadrith Mora"
+locations["18, 4"] = "Sadrith Mora"
+
+locations["1, 21"] = "Sanctus Shrine"
+
+locations["-2, -10"] = "Seyda Neen"
+locations["-2, -9"] = "Seyda Neen"
+
+locations["-20, 25"] = "Skaal Village"
+locations["-20, 26"] = "Skaal Village"
+
+locations["-23, 23"] = "Solstheim, Altar of Thrond"
+locations["-23, 20"] = "Solstheim, Brodir Grove"
+locations["-24, 26"] = "Solstheim, Castle Karstaag"
+locations["-17, 24"] = "Solstheim, Gyldenhul Barrow Entrance"
+locations["-24, 25"] = "Solstheim, Hrothmund's Bane"
+locations["-25, 23"] = "Solstheim, Hvitkald Peak"
+locations["-20, 23"] = "Solstheim, Lake Fjalding"
+locations["-21, 23"] = "Solstheim, Lake Fjalding"
+locations["-25, 26"] = "Solstheim, Mortrag Glacier"
+locations["-25, 27"] = "Solstheim, Mortrag Glacier"
+locations["-26, 26"] = "Solstheim, Mortrag Glacier"
+locations["-26, 27"] = "Solstheim, Mortrag Glacier"
+locations["-27, 22"] = "Solstheim, Thormoor's Watch"
+
+locations["6, -6"] = "Suran"
+locations["6, -7"] = "Suran"
+
+locations["15, 5"] = "Tel Aruhn"
+locations["14, -13"] = "Tel Branora"
+locations["15, -13"] = "Tel Branora"
+locations["15, 1"] = "Tel Fyr"
+locations["13, 14"] = "Tel Mora"
+locations["10, 14"] = "Tel Vos"
+
+locations["9, -7"] = "Telasero"
+
+locations["-19, 23"] = "Thirsk"
+
+locations["4, 9"] = "Tureynulal"
+
+locations["-4, 18"] = "Urshilaku Camp"
+
+locations["10, 1"] = "Uvirith's Grave"
+
+locations["-1, 18"] = "Valenvaryon"
+
+locations["-4, 18"] = "Urshilaku Camp"
+
+locations["0, 22"] = "Vas"
+
+locations["0, 10"] = "Vemynal"
+
+locations["2, -10"] = "Vivec"
+locations["3, -9"] = "Vivec"
+locations["5, -10"] = "Vivec"
+locations["4, -11"] = "Vivec, Arena"
+locations["3, -10"] = "Vivec, Foreign Quarter"
+locations["4, -10"] = "Vivec, Foreign Quarter"
+locations["2, -11"] = "Vivec, Hlaalu"
+locations["3, -11"] = "Vivec, Redoran"
+locations["3, -12"] = "Vivec, St. Delyn"
+locations["4, -12"] = "Vivec, St. Olms"
+locations["5, -11"] = "Vivec, Telvanni"
+locations["3, -13"] = "Vivec, Temple"
+locations["3, -14"] = "Vivec, Temple"
+locations["4, -13"] = "Vivec, Temple"
+locations["4, -14"] = "Vivec, Temple"
+
+locations["11, 14"] = "Vos"
+locations["12, 13"] = "Vos"
+
+locations["18, 3"] = "Wolverine Hall"
+
+locations["12, 4"] = "Yansirramus"
+
+locations["9, 10"] = "Zainab Camp"
+
+locations["12, -10"] = "Zaintiraris"
+
+locations["5, 15"] = "Zergonipal"
+
+return locations
